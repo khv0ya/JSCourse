f8af55e6c2f2afc8037bb195e2549525a187c54b
Yuliya <yuliya_filanovich_a1qa@whirlpool.com>
2019-04-27 22:16:27 +0300

    homework w2

---
+++ utils/wait.util.js
@@ -0,0 +1,37 @@
+const logger = require('../utils/log.util');
+
+function doWait(action, interval, expectedValue) {
+    return new Promise((resolve, reject) => {
+        if(action() === expectedValue) {
+            setTimeout(() => resolve(), interval); 
+        } 
+        setTimeout(() => reject(), interval); 
+    });
+}
+
+function retrier(action, maxCount, interval, expectedValue) {
+    maxCount--;
+    return doWait(action, interval, expectedValue).then(() => { 
+        logger.info('Able to reach the expected condition');
+        return true;
+}, () => {
+        if(maxCount <= 0) {
+            logger.warn(`Not able to reach the expected condition as actual value ${action()} isn't equal to expected value ${expectedValue}`);
+            return false;
+        } else {
+            return retrier(action, maxCount, interval, expectedValue);
+        }
+    });
+}
+
+class Wait {
+    forTrue(action, maxCount, interval) {
+        return retrier(action, maxCount, interval, true);
+    }
+
+    forFalse(action, maxCount, interval) {
+        return retrier(action, maxCount, interval, false);
+    }
+}
+
+module.exports = Wait;
